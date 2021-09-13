166456309d705c0987afcecca70293547ce328b2
erinakibria <erinakibria@gmail.com>
2021-09-13 00:53:32 -0600

    my first file upload

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+class FirstProgram {
+	public static void main(String args[]) {
+		System.out.println("someone put a pineapple in my school toilet");
+		System.out.println(args[0]+ ""+args[1]);
+		method1();
+		method2();		
+        } //main method
+
+	public static void method1(){
+		System.out.println("I am method 1");
+	} //method1	
+
+	public static void method2(){
+		System.out.println("I am method 2");
+	} //method2
+} // class
---
+++ P2.java
@@ -0,0 +1,8 @@
+class P2 {
+	public static void main(String args[]) {
+		
+	for(int i=0; i<10; i++){System.out.println("I am here ");}          
+		
+	} //main method
+	
+} // class P2
